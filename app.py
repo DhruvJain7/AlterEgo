e8cd7ade33ba8e66420282b19316ce7819563aa6
Dhruv Jain7 <djmjtj10@gmail.com>
2026-04-03 18:14:24 +0530

    Alter Ego is Born

---
+++ app.py
@@ -0,0 +1,154 @@
+# Import the libraries
+
+import json
+import os
+
+import gradio as gr
+import requests
+from dotenv import load_dotenv
+from openai import OpenAI
+from pypdf import PdfReader
+
+load_dotenv()
+openai = OpenAI()
+
+pushover_user = os.getenv("PUSHOVER_USER")
+pushover_token = os.getenv("PUSHOVER_TOKEN")
+pushover_url = "https://api.pushover.net/1/messages.json"
+
+
+def push(message):
+    print(f"Push: {message}")
+    payload = {"user": pushover_user, "token": pushover_token, "message": message}
+    requests.post(pushover_url, data=payload)
+
+
+def record_user_details(email, name="Name not provided", notes="not provided"):
+    push(f"Recording interest from {name} with email {email} and notes {notes}")
+    return {"recorded": "ok"}
+
+
+def record_unknown_question(question):
+    push(f"Recording {question} asked that I couldn't answer")
+    return {"recorded": "ok"}
+
+
+record_user_details_json = {
+    "name": "record_user_details",
+    "description": "Use this tool to record that a user is interested in being in touch and provided an email address",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "email": {
+                "type": "string",
+                "description": "The email address of this user",
+            },
+            "name": {
+                "type": "string",
+                "description": "The user's name, if they provided it",
+            },
+            "notes": {
+                "type": "string",
+                "description": "Any additional information about the conversation that's worth recording to give context",
+            },
+        },
+        "required": ["email"],
+        "additionalProperties": False,
+    },
+}
+
+record_unknown_question_json = {
+    "name": "record_unknown_question",
+    "description": "Always use this tool to record any question that couldn't be answered as you didn't know the answer",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "question": {
+                "type": "string",
+                "description": "The question that couldn't be answered",
+            },
+        },
+        "required": ["question"],
+        "additionalProperties": False,
+    },
+}
+
+tools = [
+    {"type": "function", "function": record_user_details_json},
+    {"type": "function", "function": record_unknown_question_json},
+]
+
+
+def handle_tools_calls(tool_calls):
+    results = []
+    for toolcall in tool_calls:
+        tool_name = toolcall.function.name
+        arguments = json.loads(toolcall.function.arguments)
+        print(f"Tool called: {tool_name}, flush=True")
+
+        if tool_name == "record_user_details":
+            result = record_user_details(**arguments)
+        elif tool_name == "record_unknown_question":
+            result = record_unknown_question(**arguments)
+
+        results.append(
+            {
+                "role": "tool",
+                "content": json.dumps(result),
+                "tool_call_id": toolcall.id,
+            }
+        )
+    return results
+
+
+reader = PdfReader("me/me.pdf")
+me = ""
+for page in reader.pages:
+    text = page.extract_text()
+    if text:
+        me += text
+
+with open("me/summary.txt", "r", encoding="utf-8") as f:
+    summary = f.read()
+
+name = "Dhruv Jain"
+
+system_prompt = f"You are acting as {name}. You are answering questions on {name}'s website, \
+particularly questions related to {name}'s career, background, skills and experience. \
+Your responsibility is to represent {name} for interactions on the website as faithfully as possible. \
+You are given a summary of {name}'s background and LinkedIn profile which you can use to answer questions. \
+Be professional and engaging, as if talking to a potential client or future employer who came across the website. \
+If you don't know the answer to any question, use your record_unknown_question tool to record the question that you couldn't answer, even if it's about something trivial or unrelated to career. \
+If the user is engaging in discussion, try to steer them towards getting in touch via email; ask for their email and record it using your record_user_details tool. "
+
+system_prompt += f"\n\n## Summary:\n{summary}\n\n## LinkedIn Profile:\n{me}\n\n"
+system_prompt += f"With this context, please chat with the user, always staying in character as {name}."
+
+
+def chat(message, history):
+    messages = (
+        [{"role": "system", "content": system_prompt}]
+        + history
+        + [{"role": "user", "content": message}]
+    )
+    done = False
+
+    while not done:
+        response = openai.chat.completions.create(
+            model="gpt-4o-mini", messages=messages, tools=tools
+        )
+
+        finish_reason = response.choices[0].finish_reason
+
+        if finish_reason == "tool_calls":
+            message = response.choices[0].message
+            tool_calls = message.tool_calls
+            results = handle_tools_calls(tool_calls)
+            messages.append(message)
+            messages.extend(results)
+        else:
+            done = True
+    return response.choices[0].message.content
+
+
+gr.ChatInterface(chat).launch()
